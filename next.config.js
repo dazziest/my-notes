62903786fef06787bd69d74ee87344fc936e6363
Aziz Muhammad <aziz@xtramilesolutions.com.au>
2022-01-02 21:22:58 +1100

    Update

--- next.config.js
+++ next.config.js
@@ -1,5 +1,3 @@
-const isProd = process.env.NODE_ENV === 'production'
-
 const withNextra = require('nextra')({
   theme: 'nextra-theme-docs',
   themeConfig: './theme.config.js',
@@ -7,7 +5,7 @@ const withNextra = require('nextra')({
 
 /** @type {import('next').NextConfig} */
 module.exports = withNextra({
-  assetPrefix: isProd ? 'https://dazziest.github.io' : '',
+  basePath: '/my-notes',
   // i18n: {
   //   locales: ['en', 'id'],
   //   defaultLocale: 'en'
